c1f78f0a390fc235f8e2e4f4354b90274185062e
Neil Traft <ntraft@gmail.com>
2013-11-23 19:41:47 -0800

    Initial grasp and lift program. Just navigates to the two necessary positions.

--- main.cpp
+++ main.cpp
@@ -1,23 +1,9 @@
-/*
- * ex08_teach_and_play.cpp
- *
- *  Created on: Sep 29, 2009
- *      Author: dc
- */
 
 #include <iostream>
-#include <vector>
 #include <string>
 
-#include <boost/ref.hpp>
-#include <boost/bind.hpp>
-#include <boost/tuple/tuple.hpp>
-
-#include <barrett/detail/stl_utils.h>  // waitForEnter()
-#include <barrett/math.h>
 #include <barrett/units.h>
 #include <barrett/systems.h>
-#include <barrett/log.h>
 #include <barrett/products/product_manager.h>
 
 #include <barrett/standard_main_function.h>
@@ -29,90 +15,84 @@ using systems::connect;
 using systems::disconnect;
 using systems::reconnect;
 
+BARRETT_UNITS_TEMPLATE_TYPEDEFS(7u);
 
+double gp[] = {0, 0.713, 0, 2.211, 0, -1.458, 0};
+double lp[] = {0, 0.437, 0, 2.1, 0, -1.028, 0};
+jp_type graspPos = jp_type(gp);
+jp_type liftPos = jp_type(lp);
+
+
+void printMenu() {
+	printf("Commands:\n");
+	printf("  [Enter]\tInitiate a grasp\n");
+	printf("  h\t\tMove to the home position\n");
+	printf("  i\t\tIdle (release position/orientation constraints)\n");
+	printf("  q\t\tQuit\n");
+}
+
+template<size_t DOF>
+void sleep(unsigned int ms) {
+	boost::this_thread::sleep(boost::posix_time::milliseconds(ms));
+}
+
+template<size_t DOF>
+void blockingMove(systems::Wam<DOF>& wam, jp_type pos) {
+	std::cout << "Move to: " << wam.getHomePosition() << std::endl;
+//	wam.moveTo(pos);
+	printf("Waiting for move to complete...\n");
+	while (!wam.moveIsDone()) {
+		sleep(10);
+	}
+	printf("Done.\n");
+}
+
+template<size_t DOF>
+void graspAndLift(systems::Wam<DOF>& wam) {
+	blockingMove(wam, graspPos);
+//	sleep(1000);
+	blockingMove(wam, liftPos);
+//	sleep(1000);
+	blockingMove(wam, graspPos);
+}
 
 template<size_t DOF>
 int wam_main(int argc, char** argv, ProductManager& pm, systems::Wam<DOF>& wam) {
-	BARRETT_UNITS_TEMPLATE_TYPEDEFS(DOF);
-	typedef boost::tuple<double, jp_type> jp_sample_type;
-
-	char tmpFile[] = "/tmp/btXXXXXX";
-	if (mkstemp(tmpFile) == -1) {
-		printf("ERROR: Couldn't create temporary file!\n");
-		return 1;
-	}
-
-	const double T_s = pm.getExecutionManager()->getPeriod();
-
 
 	wam.gravityCompensate();
 
-	systems::Ramp time(pm.getExecutionManager());
+	printMenu();
 
-	systems::TupleGrouper<double, jp_type> jpLogTg;
+	std::string line;
+	bool going = true;
+	while (going) {
+		char c = std::getchar();
 
-	// Record at 1/10th of the loop rate
-	systems::PeriodicDataLogger<jp_sample_type> jpLogger(pm.getExecutionManager(),
-			new barrett::log::RealTimeWriter<jp_sample_type>(tmpFile, 10*T_s), 10);
+		switch (c) {
+		case '\n':
+			graspAndLift(wam);
+			break;
 
+		case 'h':
+			std::cout << "Moving to home position: " << wam.getHomePosition() << std::endl;
+			wam.moveHome();
+			break;
 
-	printf("Press [Enter] to start teaching.\n");
-	waitForEnter();
-	{
-		// Make sure the Systems are connected on the same execution cycle
-		// that the time is started. Otherwise we might record a bunch of
-		// samples all having t=0; this is bad because the Spline requires time
-		// to be monotonic.
-		BARRETT_SCOPED_LOCK(pm.getExecutionManager()->getMutex());
+		case 'i':
+			printf("WAM idled.\n");
+			wam.idle();
+			break;
 
-		connect(time.output, jpLogTg.template getInput<0>());
-		connect(wam.jpOutput, jpLogTg.template getInput<1>());
-		connect(jpLogTg.output, jpLogger.input);
-		time.start();
-	}
-
-	printf("Press [Enter] to stop teaching.\n");
-	waitForEnter();
-	jpLogger.closeLog();
-	disconnect(jpLogger.input);
-
-
-	// Build spline between recorded points
-	log::Reader<jp_sample_type> lr(tmpFile);
-	std::vector<jp_sample_type> vec;
-	for (size_t i = 0; i < lr.numRecords(); ++i) {
-		vec.push_back(lr.getRecord());
-	}
-	math::Spline<jp_type> spline(vec);
-
-	printf("Press [Enter] to play back the recorded trajectory.\n");
-	waitForEnter();
-
-	// First, move to the starting position
-	wam.moveTo(spline.eval(spline.initialS()));
-
-	// Then play back the recorded motion
-	time.stop();
-	time.setOutput(spline.initialS());
-
-	systems::Callback<double, jp_type> trajectory(boost::ref(spline));
-	connect(time.output, trajectory.input);
-	wam.trackReferenceSignal(trajectory.output);
-
-	time.start();
-
-	while (trajectory.input.getValue() < spline.finalS()) {
-		usleep(100000);
+		case 'q':
+		case 'x':
+			printf("Quitting.\n");
+			going = false;
+			break;
+		}
 	}
 
 
-	printf("Press [Enter] to idle the WAM.\n");
-	waitForEnter();
 	wam.idle();
-
-
-	std::remove(tmpFile);
 	pm.getSafetyModule()->waitForMode(SafetyModule::IDLE);
-
 	return 0;
 }
